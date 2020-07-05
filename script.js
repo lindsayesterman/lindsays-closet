621fc1121e8f382af3a1cbee88bd7753653da706
Lindsay Esterman <lindsayesterman@gmail.com>
2020-07-05 11:59:34 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2500); 
+}
